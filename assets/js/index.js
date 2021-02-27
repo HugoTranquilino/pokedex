0a0483b286e010ca23689ac3f3c49249504b6fc6
Hugo <hugotranquilino@gmail.com>
2021-02-27 01:21:55 -0600

    implementacion de la busqueda y agregue calidad a al front

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,32 @@
 const pokemon = document.querySelector('#pokemon');
 const name_pokemon = document.querySelector('#nombre_pokemon')
 
+
+document.getElementById("buscar").addEventListener("click",
+    function buscar() {
+        var pokemon_buscado = document.getElementById("buscador").value;
+
+        if(pokemon_buscado != ''){
+            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_buscado}`)
+                .then(response => response.json())
+                .then((data) => {
+                    // debugger
+                    let pokemon = data.sprites.other.dream_world.front_default;
+                    let name_pokemon = data.species.name;
+                    renderPokemon(pokemon)
+                    document.getElementById('nombre_pokemon').innerHTML = name_pokemon;
+                })
+                // .catch(alert('pokemon no encontrado'));
+                .catch(error => console.log(error));
+        }else{
+            alert('Debes ingresa el nombre del pokemon que deseas buscar')
+        }
+
+        // console.log(pokemon_buscado);
+
+    }
+);
+
 function renderPokemon(image) {
     pokemon.setAttribute('src', image)
 }
@@ -17,13 +43,14 @@ function renderPokemon(image) {
 //     renderPokemon(pokemon.sprites.front_default)
 // })
 
-fetch('https://pokeapi.co/api/v2/pokemon/squirtle')
-.then(response => response.json())
-.then((data) => {
-    // debugger
-    let pokemon = data.sprites.front_default;
-    let name_pokemon = data.species.name;
-    renderPokemon(pokemon)
-    // info_pokemon(name_pokemon)
-    document.getElementById('nombre_pokemon').innerHTML = name_pokemon;
-})
+// fetch('https://pokeapi.co/api/v2/pokemon/squirtle')
+// .then(response => response.json())
+// .then((data) => {
+//     // debugger
+//     // let pokemon = data.sprites.front_default;
+//     let pokemon = data.sprites.other.dream_world.front_default;
+//     let name_pokemon = data.species.name;
+//     renderPokemon(pokemon)
+//     // info_pokemon(name_pokemon)
+//     document.getElementById('nombre_pokemon').innerHTML = name_pokemon;
+// })
