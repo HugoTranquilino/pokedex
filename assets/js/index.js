813dd59f90694f0ee048b06ed49e6454edd125c6
Hugo <hugotranquilino@gmail.com>
2021-03-19 00:03:26 -0600

    despliegue de validaciones

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,28 +3,46 @@ const name_pokemon = document.querySelector('#nombre_pokemon')
 
 
 document.getElementById("buscar").addEventListener("click",
-    function buscar() {
+    async function buscar(){
         var pokemon_buscado = document.getElementById("buscador").value.toLowerCase();
-        
         if(pokemon_buscado != ''){
-            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_buscado}`)
-                .then(response => response.json())
-                .then((data) => {
-                    // debugger
-                    let pokemon = data.sprites.other.dream_world.front_default;
-                    let name_pokemon = data.species.name;
-                    renderPokemon(pokemon)
-                    document.getElementById('nombre_pokemon').innerHTML = name_pokemon;
-                })
-                // .catch(alert('pokemon no encontrado'));
-                .catch(error => console.log(error));
-        }else{
-            alert('Debes ingresa el nombre del pokemon que deseas buscar')
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_buscado}`)
+            // console.log(response.status);
+            if(response.status ==  200){
+                const pokemon_b = await response.json();
+                let pokemon = pokemon_b.sprites.other.dream_world.front_default;
+                let name_pokemon = pokemon_b.species.name;
+                renderPokemon(pokemon)
+                document.getElementById('nombre_pokemon').innerHTML = name_pokemon;
+                console.log(pokemon);
+            }else{
+                console.log(pokemon_buscado)
+                alert('pokemon no encontrado')
+            }
         }
-
-        // console.log(pokemon_buscado);
-
     }
+    // function buscar() {
+    //     var pokemon_buscado = document.getElementById("buscador").value.toLowerCase();
+        
+    //     if(pokemon_buscado != ''){
+    //         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_buscado}`)
+    //             .then(response => response.json())
+    //             .then((data) => {
+    //                 // debugger
+    //                 let pokemon = data.sprites.other.dream_world.front_default;
+    //                 let name_pokemon = data.species.name;
+    //                 renderPokemon(pokemon)
+    //                 document.getElementById('nombre_pokemon').innerHTML = name_pokemon;
+    //             })
+    //             // .catch(alert('pokemon no encontrado'));
+    //             .catch(error => console.log(error));
+    //     }else{
+    //         alert('Debes ingresa el nombre del pokemon que deseas buscar')
+    //     }
+
+    //     // console.log(pokemon_buscado);
+
+    // }
 );
 
 function renderPokemon(image) {
